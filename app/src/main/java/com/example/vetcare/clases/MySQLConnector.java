299f37acff8ee102cbe09bbfd390de358001736b
Andre-RM <levinromero10@gmail.com>
2024-11-04 00:49:25 -0500

    Se añadio comentarios

--- app/src/main/java/com/example/vetcare/clases/MySQLConnector.java
+++ app/src/main/java/com/example/vetcare/clases/MySQLConnector.java
@@ -4,21 +4,29 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class MySQLConnector {
+    // URL de conexión a la base de datos MySQL en el servidor remoto, incluyendo el nombre de la base de datos "VetCare"
     private static final String URL = "jdbc:mysql://54.87.3.7:3306/VetCare";
+    // Nombre de usuario para autenticar la conexión a la base de datos
     private static final String USER = "admin";
+    // Contraseña del usuario para la conexión
     private static final String PASSWORD = "abc123$";
 
+    // Método para probar la conexión a la base de datos
     public int probarConexion() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver"); // Carga el driver MySQL
+            // Carga el driver de MySQL para permitir la conexión a la base de datos
+            Class.forName("com.mysql.jdbc.Driver");
+            // Establece la conexión usando la URL, usuario y contraseña proporcionados
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             return 1; // Conexión exitosa
         } catch (Exception e) {
+            // Imprime cualquier error de conexión en la consola
             e.printStackTrace();
             return 0; // Error en la conexión
         } finally {
             try {
+                // Cierra la conexión si fue establecida, para liberar los recursos
                 if (connection != null) connection.close();
             } catch (Exception e) {
                 e.printStackTrace();
@@ -26,16 +34,19 @@ public class MySQLConnector {
         }
     }
 
+    // Método para obtener una conexión a la base de datos
     public Connection conecta(){
         Connection connection = null;
         try {
+            // Carga el driver de MySQL
             Class.forName("com.mysql.jdbc.Driver"); // Carga el driver MySQL
+            // Intenta establecer la conexión a la base de datos
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
             return connection;
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        return null; // Retorna null si no se pudo establecer la conexión
     }
 }
 
